dc51b382ea2e0d5358a65593445d2210599f938d
Dorra Abidi <dorra.abidi@esprit.tn>
2021-07-24 18:24:29 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RequestCreateComponent } from './components/request-create/request-create.component';
+import { RequestDetailComponent } from './components/request-detail/request-detail.component';
+import { RequestListComponent } from './components/request-list/request-list.component';
+import { RequestUpdateComponent } from './components/request-update/request-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'request/list', component: RequestListComponent },
+  { path: 'client/:id/request/list', component: RequestListComponent },
+  { path: 'request/edit/:id', component: RequestUpdateComponent },
+  { path: 'request/detail/:id', component: RequestDetailComponent },
+  { path: 'request/create', component: RequestCreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RequestListComponent } from './components/request-list/request-list.component';
+import { RequestCreateComponent } from './components/request-create/request-create.component';
+import { RequestDetailComponent } from './components/request-detail/request-detail.component';
+import { RequestUpdateComponent } from './components/request-update/request-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RequestListComponent,
+    RequestCreateComponent,
+    RequestDetailComponent,
+    RequestUpdateComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
